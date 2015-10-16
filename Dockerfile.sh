8777adc7b6bc1efb236cf0ffb961c5a64e79856b
Annakan <watever@example.com>
2015-10-16 23:46:09 +0000

    tweaks

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -2,7 +2,20 @@
 # Clone from the Fedora 22 image
 #FROM fedora:22
 
-MAINTAINER Jan Pazdziora
+# find script true place
+# from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+yum install -y dnf
+
+pushd $DIR
 
 # Install FreeIPA server
 mkdir -p /run/lock ; dnf install -y freeipa-server bind bind-dyndb-ldap perl && dnf clean all
@@ -23,6 +36,9 @@ groupadd -g 389 dirsrv ; useradd -u 389 -g 389 -c 'DS System User' -d '/var/lib/
 cp volume-data-list /etc/volume-data-list
 cp volume-data-mv-list /etc/volume-data-mv-list
 set -e ; cd / ; mkdir /data-template ; cat /etc/volume-data-list | while read i ; do echo $i ; if [ -e $i ] ; then tar cf - .$i | ( cd /data-template && tar xf - ) ; fi ; mkdir -p $( dirname $i ) ; if [ "$i" == /var/log/ ] ; then mv /var/log /var/log-removed ; else rm -rf $i ; fi ; ln -sf /data${i%/} ${i%/} ; done
+
+cd $DIR
+
 cp volume-data-autoupdate /etc/volume-data-autoupdate
 rm -rf /var/log-removed
 mv /data-template/etc/dirsrv/schema /usr/share/dirsrv/schema && ln -s /usr/share/dirsrv/schema /data-template/etc/dirsrv/schema
@@ -35,3 +51,5 @@ uuidgen > /data-template/build-id
 
 #ENTRYPOINT /usr/sbin/ipa-server-configure-first
 ./ipa-server-configure-first
+
+popd  
